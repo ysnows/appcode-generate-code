e42281e8e764c478be83a4d6904f6316dbf8e3be
ysnows <ysnows@163.com>
2021-08-20 13:45:10 +0800

    1. 生成api

--- src/main/java/editItem/EditItemViewDialog.java
+++ src/main/java/editItem/EditItemViewDialog.java
@@ -48,7 +48,7 @@ public class EditItemViewDialog extends JDialog implements KeyListener, Document
         btnGenerate.addActionListener(e -> {
             if (onClickListener != null) {
                 String name = CommonUtil.toUpperCase4Index(tfname.getText());
-                onClickListener.onGenerate(name, tfFont.getText(), tfColor.getText(), tfText.getText(), tfSubFont.getText(), tfSubColor.getText(), tfSubText.getText(), tftfEditable.getText(), tfArrow.getText(), tfLine.getText(), tfHeight.getText(),tfMasory.getText());
+                onClickListener.onGenerate(name, tfFont.getText(), tfColor.getText(), tfText.getText(), tfSubFont.getText(), tfSubColor.getText(), tfSubText.getText(), tftfEditable.getText(), tfArrow.getText(), tfLine.getText(), tfHeight.getText(), tfMasory.getText());
             }
             dispose();
         });
@@ -91,6 +91,63 @@ public class EditItemViewDialog extends JDialog implements KeyListener, Document
         //关键是下面这两行代码
         Document document = tfname.getDocument();
         document.addDocumentListener(this);
+
+        tfMasory.getDocument().addDocumentListener(new DocumentListener() {
+            @Override
+            public void insertUpdate(DocumentEvent documentEvent) {
+                var text = tfMasory.getText();
+                if (text.contains("|")) {
+
+                    var strArr = text.split("\\|");
+
+                    var font = strArr[0];
+                    var colorStr = strArr[1];
+                    var colorArr = colorStr.split(",");
+                    var color = colorArr[0];
+
+                    var whStr = strArr[2];
+                    var whArr = whStr.split(",");
+                    var width = whArr[0];
+                    var height = whArr[1];
+
+                    var content = strArr[3];
+                    var radius = strArr[4];
+                    var bgColorStr = strArr[5];
+                    var bgColorArr = bgColorStr.split(",");
+                    var bgColor = bgColorArr[0];
+
+                    var borderWidth = strArr[6];
+
+                    var borderColorStr = strArr[7];
+                    var borderColorArr = borderColorStr.split(",");
+                    var borderColor = borderColorArr[0];
+
+
+                    if (!font.equals("0")) tfSubFont.setText(font);
+                    if (!color.equals("0")) tfSubColor.setText(color);
+                    if (!content.equals("0")) tfSubText.setText(content);
+
+                    Timer timer = new Timer(100, new ActionListener() {
+                        @Override
+                        public void actionPerformed(ActionEvent actionEvent) {
+                            tfMasory.setText("");
+                        }
+                    });
+                    timer.setRepeats(false);
+                    timer.start();
+                }
+            }
+
+            @Override
+            public void removeUpdate(DocumentEvent documentEvent) {
+
+            }
+
+            @Override
+            public void changedUpdate(DocumentEvent documentEvent) {
+
+            }
+        });
     }
 
     @Override
@@ -98,6 +155,8 @@ public class EditItemViewDialog extends JDialog implements KeyListener, Document
         System.out.println("insert text");
 
 //        String text = "12|#2A2A2A,100%|136,12|联系电话：15098119021|0";
+
+
         var text = tfname.getText();
         if (text.contains("|")) {
 
@@ -140,48 +199,7 @@ public class EditItemViewDialog extends JDialog implements KeyListener, Document
             timer.setRepeats(false);
             timer.start();
         }
-        text = tfSubFont.getText();
-        if (text.contains("|")) {
 
-            var strArr = text.split("\\|");
-
-            var font = strArr[0];
-            var colorStr = strArr[1];
-            var colorArr = colorStr.split(",");
-            var color = colorArr[0];
-
-            var whStr = strArr[2];
-            var whArr = whStr.split(",");
-            var width = whArr[0];
-            var height = whArr[1];
-
-            var content = strArr[3];
-            var radius = strArr[4];
-            var bgColorStr = strArr[5];
-            var bgColorArr = bgColorStr.split(",");
-            var bgColor = bgColorArr[0];
-
-            var borderWidth = strArr[6];
-
-            var borderColorStr = strArr[7];
-            var borderColorArr = borderColorStr.split(",");
-            var borderColor = borderColorArr[0];
-
-
-            if (!font.equals("0")) tfSubFont.setText(font);
-            if (!color.equals("0")) tfSubColor.setText(color);
-            if (!content.equals("0")) tfSubText.setText(content);
-
-            Timer timer = new Timer(100, new ActionListener() {
-                @Override
-                public void actionPerformed(ActionEvent actionEvent) {
-                    tfFont.setText("");
-                    tftfEditable.requestFocus();
-                }
-            });
-            timer.setRepeats(false);
-            timer.start();
-        }
     }
 
     @Override
--- src/main/java/vmfield/FieldParseUtil.java
+++ src/main/java/vmfield/FieldParseUtil.java
@@ -18,16 +18,15 @@ public class FieldParseUtil {
         for (String str : arrField) {
             var arrFieldInfo = str.split(":");
             var fieldName = arrFieldInfo[0];
-            var fieldType = getFieldType(arrFieldInfo[1]);
+            var fieldType = getFieldType(arrFieldInfo.length > 1 ? arrFieldInfo[1] : "str");
 
-            propertyBuilder.append("\n@property(nonatomic, copy) ");
+            propertyBuilder.append("\n@property(nonatomic, ");
             propertyBuilder.append(fieldType);
             propertyBuilder.append(fieldName);
             propertyBuilder.append(";");
 
-            observerBuilder.append("\n\t[RACObserve(self.viewModel, " + fieldName + ") subscribeNext:^(id x) {");
-            observerBuilder.append("\n\t\tif (" + fieldName + " != nil) {");
-            observerBuilder.append("\n\t\tif (" + fieldName + " != nil) {");
+            observerBuilder.append("\n\t[RACObserve(self.viewModel, ").append(fieldName).append(") subscribeNext:^(").append(fieldType).append(fieldName).append(") {");
+            observerBuilder.append("\n\t\tif (").append(fieldName).append(" != nil) {");
             observerBuilder.append("\n\n\t\t}");
             observerBuilder.append("\n\t}];");
 
@@ -44,12 +43,13 @@ public class FieldParseUtil {
 
         switch (text) {
             case "int":
-                return "NSInteger\t";
+                return "assign) NSInteger\t";
             case "bool":
-                return "Boolean\t";
+                return "assign) Boolean\t";
             case "str":
+                return "copy) NSString\t*";
             default:
-                return "NSString\t\\*\t";
+                return "";
         }
     }
 }
